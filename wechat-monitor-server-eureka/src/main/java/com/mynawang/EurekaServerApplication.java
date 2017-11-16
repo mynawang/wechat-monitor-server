ae1ae503ff19f82dbd2776ea57b1a5ecdd7bf2e7
mynawang <18267853511@163.com>
2017-11-16 16:43:44 +0800

    init spring cloud

---
+++ wechat-monitor-server-eureka/src/main/java/com/mynawang/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.mynawang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by mynawang on 2017/11/16 0016.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
